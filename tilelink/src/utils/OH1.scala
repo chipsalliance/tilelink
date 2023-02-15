3da3c9297e6577cefc88d9c8046786ebbf869762
OceanS2000 <30361859+OceanS2000@users.noreply.github.com>
2023-02-15 23:50:49 +0800

    [utils] Utility functions from rocket-chip

---
+++ tilelink/src/utils/OH1.scala
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2016-2017 SiFive, Inc.
+
+package utils
+
+import chisel3._
+import chisel3.util.{Cat, OHToUInt}
+
+object OH1 {
+  def OH1ToOH(x: UInt): UInt =
+    ((x << 1).asUInt | 1.U) & (~Cat(0.U(1.W), x)).asUInt
+
+  def OH1ToUInt(x: UInt): UInt = OHToUInt(OH1ToOH(x))
+
+  def UIntToOH1(x: UInt, width: Int): UInt =
+    (~((-1).S(width.W).asUInt << x)(width - 1, 0)).asUInt
+
+  def UIntToOH1(x: UInt): UInt = UIntToOH1(x, (1 << x.getWidth) - 1)
+}
