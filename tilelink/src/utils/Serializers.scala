72c7cb54180c5d7db435988bddb1c34fb8f25d7e
OceanS2000 <30361859+OceanS2000@users.noreply.github.com>
2023-02-20 22:23:41 +0800

    [rtl] flip ports correctly

--- tilelink/src/utils/Serializers.scala
+++ tilelink/src/utils/Serializers.scala
@@ -8,7 +8,7 @@ import upickle.default.{readwriter, ReadWriter => RW}
 object Serializers {
   // Inject serializers for chisel3 BitSet and BitPat
   implicit val bitPatSerializer: RW[chisel3.util.BitPat] =
-    readwriter[String].bimap(_.rawString, chisel3.util.BitPat(_))
+    readwriter[String].bimap(_.rawString, s => { val bs: String = "b" ++ s; chisel3.util.BitPat(bs) })
   implicit val bitSetSerializer: RW[chisel3.util.experimental.BitSet] =
     readwriter[Seq[chisel3.util.BitPat]].bimap(_.terms.toSeq, chisel3.util.experimental.BitSet(_: _*))
 }
