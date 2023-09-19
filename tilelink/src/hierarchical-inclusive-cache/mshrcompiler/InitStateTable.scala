c2641c914fb7b423f829cc8cf39e69ab8d144f79
qinjun-li <liqinjun1994@gmail.com>
2023-09-19 14:42:47 +0800

    [cache] Filter out illegal situations.

--- tilelink/src/hierarchical-inclusive-cache/mshrcompiler/InitStateTable.scala
+++ tilelink/src/hierarchical-inclusive-cache/mshrcompiler/InitStateTable.scala
@@ -26,8 +26,23 @@ object InitStateTable {
       (ds.hit, ds.dirty, ds.state, ds.hitOtherClient, rs.channel, rs.opCode, rs.parameter)
     }.filter( rd =>
       rd match {
+        // release require Hit
         case (HitN, _:Dirty, _:State, _:ClientState, C , _: OpCode, _: Parameter) => false
-        case _ => true
+        // AcquireBlock | AcquirePerm BtoT require Hit
+        case (HitN, _: Dirty, _: State, _: ClientState, A, AcquireBlock | AcquirePerm, BtoT) => false
+        // release will definitely hit the initiator
+        case (_, _: Dirty, _: State, NoClient | HitOtherClient, C, _: OpCode, _: Parameter) => false
+        // Acquire BtoT will definitely hit the initiator
+        case (_: Hit, _: Dirty, _: State, NoClient | HitOtherClient, A, AcquirePerm | AcquireBlock, BtoT) => false
+        // release BtoX, Local can only be Tip | Branch
+        case (_: Hit, _: Dirty, Trunk | Nothing, _: ClientState, C, _: OpCode, BtoN | BtoB) => false
+        // release TtoX, ClientState can only be HitSelf
+        case (_: Hit, _: Dirty, _: State, HitAll, C, _: OpCode, TtoB | TtoT | TtoN) => false
+        // release TtoX, State can only be Trunk
+        case (_: Hit, _: Dirty, Tip | Branch | Nothing, _: ClientState, C, _: OpCode, TtoB | TtoT | TtoN) => false
+        // B X channel will not hit itself
+        case (_: Hit, _: Dirty, _: State, HitAll | HitSelf, B | X, _: OpCode, _: Parameter) => false
+        case (_: Hit, _: Dirty, _: State, _: ClientState, _: Channel, _: OpCode, _: Parameter) => true
       }
     )
 
@@ -48,9 +63,16 @@ object InitStateTable {
     }
     val sAcquire = if (needAcquire) StateN else StateY
 
+    // Does a request prove the client need not be probed?
+    val skipProbeN = o match {
+      case AcquireBlock | AcquirePerm | Get | Hint => true
+      case _ => false
+    }
+
     // probe caused by permission transfer
     val needPProb = (n, h, s, c, needT, p) match {
       case (A, HitY, Trunk, HitOtherClient | HitAll, _, _) => true
+      case (A, HitY, _, HitSelf, true, _) => !skipProbeN
       case (A, HitY, _, HitOtherClient | HitAll, true, _) => true
       // prob to N & hit client
       case (B, HitY, _, HitOtherClient | HitAll, _, ToN) => true
@@ -91,7 +113,7 @@ object InitStateTable {
       // for x channel
       // for a channel
       // Becomes dirty?
-      case (HitN, DirtyN, _: State, _: ClientState, A , PutFullData | PutPartialData | ArithmeticData | LogicalData, _: Parameter) => StateN
+      case (HitY, DirtyN, _: State, _: ClientState, A , PutFullData | PutPartialData | ArithmeticData | LogicalData, _: Parameter) => StateN
       case _ => StateY
     }
 
